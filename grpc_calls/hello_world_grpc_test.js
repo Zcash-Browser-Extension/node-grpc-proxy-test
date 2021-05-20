e7f118bb6bd45bcc29c5549424d6b0ba10a5350b
Elliot Blanchard <git@elliotblanchard.com>
2021-05-20 16:27:13 -0400

    Cleanup

--- grpc_calls/hello_world_grpc_test.js
+++ grpc_calls/hello_world_grpc_test.js
@@ -1,12 +1,12 @@
 let methods = {}
 
-methods.helloWorld = async () => {
-    var PROTO_PATH = __dirname + '/../proto/helloworld.proto';
+methods.helloWorld = function () {
+    const PROTO_PATH = __dirname + '/../proto/helloworld.proto'
 
-    var parseArgs = require('minimist');
-    var grpc = require('@grpc/grpc-js');
-    var protoLoader = require('@grpc/proto-loader');
-    var packageDefinition = protoLoader.loadSync(
+    //var parseArgs = require('minimist')
+    const grpc = require('@grpc/grpc-js')
+    const protoLoader = require('@grpc/proto-loader')
+    const packageDefinition = protoLoader.loadSync(
         PROTO_PATH,
         {keepCase: true,
          longs: String,
@@ -14,78 +14,12 @@ methods.helloWorld = async () => {
          defaults: true,
          oneofs: true
         });
-    var hello_proto = grpc.loadPackageDefinition(packageDefinition).helloworld;
+    const hello_proto = grpc.loadPackageDefinition(packageDefinition).helloworld
     
     const target = 'localhost:50051'
-    let client = new hello_proto.Greeter(target,grpc.credentials.createInsecure());
-    let user = 'world'
-    let responseMsg = 'foo'
-    
-    //client.sayHello({name: user}, function(err, response) { console.log('Greeting:', response.message) });   
-    
-    /*
-    let getGrpcRequest = (nameObj) => {
-        return new Promise(
-          (resolve, reject) => {
-            client.sayHello(nameObj, function(error, response) { 
-              if (error) reject(error)
-              let fact = response.message
-              resolve(fact)
-            })
-         }
-       )
-      };
-      
-      getGrpcRequest({name: user}).then(
-         fact => {
-             console.log(fact) // actually outputs a string
-            }
-      ).catch(
-         error => console.log(error)
-      )
-    */
+    let client = new hello_proto.Greeter(target,grpc.credentials.createInsecure())
 
     return client
-    /*  
-    client.sayHello({name: user}, function(err, response) { 
-        if(err) throw new Error("Error while fetching fetching data")
-        console.log('Greeting:', response.message) 
-    });
-    */ 
-    
- 
-
-    //console.log("hi")
-    //return 5
-    /*
-    function main() {
-      var argv = parseArgs(process.argv.slice(2), {
-        string: 'target'
-      });
-      var target;
-      if (argv.target) {
-        target = argv.target;
-      } else {
-        target = 'localhost:50051';
-      }
-      var client = new hello_proto.Greeter(target,
-                                           grpc.credentials.createInsecure());
-      var user;
-      if (argv._.length > 0) {
-        user = argv._[0]; 
-      } else {
-        user = 'world';
-      }
-      client.sayHello({name: user}, function(err, response) {
-        console.log('Greeting:', response.message);
-      });
-      client.sayHelloAgain({name: 'you'}, function(err, response) {
-        console.log('Greeting:', response.message);
-      });  
-    }
-    
-    main();
-    */
 }
 
 exports.data = methods
--- server.js
+++ server.js
@@ -1,30 +1,15 @@
 const http = require('http')
 const bodyParser = require('body-parser')
-//const helloWorld = require('./grpc_calls/hello_world_require_test.js')
 const helloWorld = require('./grpc_calls/hello_world_grpc_test.js')
 
 const server = http.createServer((req, res) => {
     if (req.url === '/') {
-        helloWorld.data.helloWorld().then((client) => {
-            
-            /*
-            return client.sayHello({name: user}, function(err, response) { 
-                if(err) throw new Error("Error while fetching fetching data")
-                console.log('Greeting:', response.message) 
-            }); 
-            */
-            
-            client.sayHello({name: 'user'}, function(err, response) { 
-                if(err) throw new Error("Error while fetching fetching data")
-                res.write(response.message)
-                res.end()
-                //console.log('Greeting:', response.message) 
-            }); 
-
-            //console.log(value)
-            //res.write(value.toString())
-            //res.end()
-        })
+        const client = helloWorld.data.helloWorld()
+        client.sayHello({name: 'Sam'}, function(err, response) { 
+            if(err) throw new Error("Error while fetching fetching data")
+            res.write(response.message)
+            res.end()
+        }) 
     }
 })
 
